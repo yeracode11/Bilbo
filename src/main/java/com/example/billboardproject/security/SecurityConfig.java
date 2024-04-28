d74d5b22428f3d10dbeee2722f7a26010804d6d6
Yersultan <55252746+yeracode11@users.noreply.github.com>
2024-04-28 14:08:56 +0600

    Update SecurityConfig.java

--- src/main/java/com/example/billboardproject/security/SecurityConfig.java
+++ src/main/java/com/example/billboardproject/security/SecurityConfig.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -58,6 +59,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-
 }
