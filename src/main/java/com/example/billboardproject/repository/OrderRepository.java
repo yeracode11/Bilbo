d59fc1b53433b5b6d7f286f1b7a173ad29b21311
Yersultan <55252746+yeracode11@users.noreply.github.com>
2024-04-30 19:08:47 +0600

    Update OrderRepository.java

--- src/main/java/com/example/billboardproject/repository/OrderRepository.java
+++ src/main/java/com/example/billboardproject/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package com.example.billboardproject.repository;
 
 import com.example.billboardproject.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,9 +12,10 @@ import java.util.List;
 @Repository
 @Transactional
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<Order> findOrdersByUser_Id(Long id);
-
-    List<Order> findOrdersByBillboard_Id(Long id);
-
-    List<Order> findAll();
+    @Query(value = "select * from order_for_billboards where user_id=:id order by status, order_date DESC", nativeQuery = true)
+    List<Order> findOrdersByUserId(Long id);
+    @Query(value = "select * from order_for_billboards where billboard_id=:id order by status, order_date DESC", nativeQuery = true)
+    List<Order> findOrdersByBillboardId(Long id);
+    @Query(value = "select * from order_for_billboards order by status, order_date DESC", nativeQuery = true)
+    List<Order> findOrdersBySorting();
 }
