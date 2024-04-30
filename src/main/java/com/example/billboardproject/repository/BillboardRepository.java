b0ff1d25e07ce1348393881a499a91a37f2d72f3
Yersultan <55252746+yeracode11@users.noreply.github.com>
2024-04-30 11:36:34 +0600

    Update BillboardRepository.java

--- src/main/java/com/example/billboardproject/repository/BillboardRepository.java
+++ src/main/java/com/example/billboardproject/repository/BillboardRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
 @Transactional
 public interface BillboardRepository extends JpaRepository<Billboard, Long> {
-  @Query(value = "select * from billboard order by created_at desc",nativeQuery = true)
+    @Query(value = "select * from billboard order by created_at desc",nativeQuery = true)
     List<Billboard> findAllOrderByCreatedAt();
 }
