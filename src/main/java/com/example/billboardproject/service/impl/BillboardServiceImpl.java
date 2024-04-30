4db4cc49fc49719cf098c36aa16e2cb82d2e4880
Yersultan <55252746+yeracode11@users.noreply.github.com>
2024-04-30 11:30:01 +0600

    Update BillboardServiceImpl.java

--- src/main/java/com/example/billboardproject/service/impl/BillboardServiceImpl.java
+++ src/main/java/com/example/billboardproject/service/impl/BillboardServiceImpl.java
@@ -17,7 +17,7 @@ public class BillboardServiceImpl implements BillboardService {
 
     @Override
     public List<Billboard> getAllBillboards() {
-        return billboardRepository.findAll();
+        return billboardRepository.findAllOrderByCreatedAt();
     }
 
     @Override
