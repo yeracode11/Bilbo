1070381163645c1b6b3fe935274e3c8a89bf5998
Yersultan <55252746+yeracode11@users.noreply.github.com>
2024-04-30 11:33:56 +0600

    Update BillboardRepository.java

--- src/main/java/com/example/billboardproject/repository/BillboardRepository.java
+++ src/main/java/com/example/billboardproject/repository/BillboardRepository.java
@@ -2,6 +2,7 @@ package com.example.billboardproject.repository;
 
 import com.example.billboardproject.model.Billboard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
